b3bb0094629fc2eb5a8c33fb8fc78739e102858f
Aldo Aranza <aldo@adndigital.mx>
2022-08-31 18:02:23 -0500

    Correccion en el proceso masivo de inscritos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -996,7 +996,13 @@ class HomeController extends Controller
                                 $alumno = Alumno::with('documentos')->find($alumno->id);
                                 $documento = $alumno->documentos('id_especialidad','=',$especialidad->id)->where('tipo','=','Inscripción')->where('status','=','Pagado')->first();
                                 if($documento){
-                                    $fecha_pago = $documento->load('abonos.pago')->abonos->first()->pago->fecha;
+                                    if($fecha_pago = $documento->load('abonos.pago')->abonos->first()){
+                                        $fecha_pago = $documento->load('abonos.pago')->abonos->first()->pago->fecha;
+                                    }else{
+                                        $grupo = $alumno->grupos->where('id_especialidad',$especialidad->id)->first();
+                                        $fecha_pago = ($especialidad->pivot->fecha_inicio)? $especialidad->pivot->fecha_inicio:$grupo->fecha_inicio;
+                                    }
+                                    
                                 }else{
                                     $grupo = $alumno->grupos->where('id_especialidad',$especialidad->id)->first();
                                     $fecha_pago = ($especialidad->pivot->fecha_inicio)? $especialidad->pivot->fecha_inicio:$grupo->fecha_inicio;
