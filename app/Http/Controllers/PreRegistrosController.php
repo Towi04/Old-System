be96f23bcf044b379bd60c06917c1f9a4b91b524
Aldo Aranza <aldo@adndigital.mx>
2022-09-23 18:57:25 -0500

    Se pone el get file para saber de donde viene la excepcion

--- app/Http/Controllers/PreRegistrosController.php
+++ app/Http/Controllers/PreRegistrosController.php
@@ -512,7 +512,7 @@ class PreRegistrosController extends Controller
             // dd($th);
             return response()->json([
                 'success'   => false,
-                'message'   => 'Ocurrio el siguiente error:' .$th->getMessage().' en la línea '.$th->getLine(),
+                'message'   => 'Ocurrio el siguiente error:' .$th->getMessage().' en la línea '.$th->getLine().' en el archivo '.$th->getFile(),
                 // 'redirect'  => route('alumnos.show',$alumno),
                 // 'pago'      => $request->has('id_grupo') ? Pago::first()->where('id_alumno',$alumno->id)->latest()->first() : ''
             ]);
