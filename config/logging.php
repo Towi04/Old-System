5a72d718582be0762fb8e053b759195ccc6a9582
Aldo Aranza <aldo@adndigital.mx>
2023-06-06 10:03:18 -0600

    Se reconfigura el daiyly pero se deja en stack por que es mas facil revisarlo ahí

--- config/logging.php
+++ config/logging.php
@@ -56,9 +56,10 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('app/public/actividad.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
+            'ignore_exceptions' => true,
         ],
 
         'slack' => [
