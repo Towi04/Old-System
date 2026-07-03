455ecd7c3368561f877c9cdb7c11f0f9f2ba53cb
Towi04 <englishcoordinator@cncm.edu.mx>
2026-07-03 17:14:00 -0600

    update config

--- config.php
+++ config.php
@@ -2,11 +2,16 @@
 
 /**
  * Configuracion de conexion a base de datos.
- * Servidor: cncm.edu.mx
+ *
+ * Escenario actual: sistema en tu PC, base de datos en cncm.edu.mx.
+ * - Usa host cncm.edu.mx
+ * - En cPanel habilita "Remote MySQL" y agrega tu IP publica
+ *
+ * Cuando subas el sistema al mismo servidor de CNCM, cambia host a localhost.
  */
 return [
     'connection' => 'mysql',
-    'host'       => 'cncm.edu.mx',
+    'host'       => 'localhost',
     'port'       => 3306,
     'database'   => 'cncmedum_legado',
     'username'   => 'cncmedum_tovar',
